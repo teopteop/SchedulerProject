1ae42334ce6722df48623682942e71de2fcc54aa
teopteop <teopteop0718@gmail.com>
2025-05-13 20:59:11 +0900

    feat: add user table refactor: add relationship between User and Schedule entities

---
+++ schedule.sql
@@ -0,0 +1,19 @@
+create table schedule
+(
+    scheduleId bigint auto_increment primary key,
+    authorId bigint not null comment "userid 참조키",
+    password varchar(20) not null,
+    task varchar(500) not null,
+    createDate timestamp default current_timestamp,
+    lastModifiedDate timestamp default current_timestamp on update  current_timestamp,
+    foreign key (authorId) references user(userId)
+);
+
+create table user
+(
+    userId bigint auto_increment primary key,
+    name varchar(20) not null,
+    email varchar(50) not null,
+    createDate timestamp default current_timestamp,
+    lastModifiedDate timestamp default current_timestamp on update  current_timestamp
+);
