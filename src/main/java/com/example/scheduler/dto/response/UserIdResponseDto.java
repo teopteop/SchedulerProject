f42c4fd18a72b3ef3a862525782150bfcaad25ae
teopteop <teopteop0718@gmail.com>
2025-05-14 03:19:13 +0900

    feat: add Constructor

---
+++ src/main/java/com/example/scheduler/dto/response/UserIdResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.scheduler.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserIdResponseDto {
+    private Long userId;
+}
