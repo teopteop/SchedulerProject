fb5c4157d519711ebc226ae7bb6d852baa30905b
teopteop <teopteop0718@gmail.com>
2025-05-13 17:40:14 +0900

    feat: add PasswordResponseDto

---
+++ src/main/java/com/example/scheduler/dto/response/PasswordResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.scheduler.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PasswordResponseDto {
+    private Long id;
+    private String password;
+}
