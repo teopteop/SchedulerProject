e197365ca5d89292f6188975e2da943453b3d9bd
teopteop <teopteop0718@gmail.com>
2025-05-14 13:22:31 +0900

    feat: add @setter annotation

--- src/main/java/com/example/scheduler/dto/response/ScheduleResponseDto.java
+++ src/main/java/com/example/scheduler/dto/response/ScheduleResponseDto.java
@@ -2,10 +2,12 @@ package com.example.scheduler.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class ScheduleResponseDto {
 
