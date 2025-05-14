917f7f4edf41150f9d4060aeb54d053d14d7afc4
teopteop <teopteop0718@gmail.com>
2025-05-14 13:35:30 +0900

    fix: return appropriate HTTP status codes for exceptions

--- src/main/java/com/example/scheduler/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/scheduler/exception/GlobalExceptionHandler.java
@@ -21,16 +21,18 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
+    //인증실패
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handlePasswordMismatchException(PasswordMismatchException ex){
-        ErrorResponse response = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), HttpStatus.UNAUTHORIZED.value());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 
+    //로직상 예외가 발생할 일은 없지만 예외가 터진경우 서버나 DB의 문제일 확률이 있음 500에러 리턴
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleQueryFailedException(QueryFailedException ex){
-        ErrorResponse response = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
 }
